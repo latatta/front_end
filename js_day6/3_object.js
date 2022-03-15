e5d6fc3a42c168092f6df2523de9b60e1c6ec57a
kimsohee <koreait.2021@gmail.com>
2022-03-15 10:24:40 +0900

    day7_form값읽어오기

--- js_day6/3_object.js
+++ js_day6/3_object.js
@@ -11,10 +11,11 @@ let person = { name:'sana',
                age:19,
                sayHi : function(){    //메소드 정의:객체의 프로퍼티값이 함수 정의일때 메소드 입니다.
                  console.log(`Hi!! 나는 ${this.name} 입니다.`);   
+                 console.log('Hi!! 나는' + this.name + '입니다.');   
                } 
             };        //객체리터럴을 이용하여 객체 생성
 
-person.sayHi();     //객체의 메소드 실행
+person.sayHi();     //객체의 메소드 실행  , this는 person변수가 참조하는 객체.
 
 //프로퍼티 접근하여 person 객체의 이름변경
 person.name='사나';     //방법1
@@ -22,3 +23,15 @@ person.sayHi();
 
 person['name']='momo';  //방법2
 person.sayHi();
+
+person.color = 'red';       //새로운 프로퍼티를 바로 추가합니다.
+console.log(person);        //{name: 'momo', age: 19, sayHi: ƒ, color: 'red'}
+
+let person2 = { name:'정연',
+    age:19,
+    sayHi(){    //메소드 정의 : function 생략가능합니다.
+        console.log(`Hi!! 나는 ${this.name} 입니다.`);   
+    } 
+}; 
+
+person2.sayHi();
---
+++ js_day6/4_string메소드.js
@@ -0,0 +1,24 @@
+/* string 메소드 정리 완료하면 구글 폴더에 올려주세요. 9시 30분까지.
+
+-string 주요 메소드
+
+slice(start,end)  : start,end 인덱스가 음수가 될수 있음.이때는 맨끝 부터 -1
+substring(start,end)  : end 인덱스 문자는 포함 안함
+substr(start,length)  : 두번재 인자가 추출할 길이
+replace(src,dest)   : src문자열을 dest로 대체함.
+toUpperCase()
+toLowerCase()
+concat(join,str2) :  str2문자열과 연결함. join문자열은 연결할때 사용할 문자열
+padStart()  : 예제 참고
+padEnd() : 예제 참고
+charAt(index) : index위치의 문자 구하기, 
+     - 자바스크립트에서는 문자열을 배열처럼 다룰수 있음. [] 안에 인덱스지정 가능.
+split() : 문자열 나누기(구분기호 사용)
+
+문자열검색 메소드 : 사용형식은 자바와 동일.
+String indexOf()
+String lastIndexOf()
+String startsWith()
+String endsWith()
+
+*/
