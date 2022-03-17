9cb5504e44c10d9935edaf64b111eae8c175d1c0
kimsohee <koreait.2021@gmail.com>
2022-03-17 14:18:55 +0900

    day8_퇴실이벤트추가

--- js_day8/2_dateMethod.js
+++ js_day8/2_dateMethod.js
@@ -6,7 +6,7 @@ console.log(new Date())
 console.log(new Date(2002,05,01))  //날짜 지정 : Sat Jun 01 2002 00:00:00 GMT+0900 (대한민국 표준시)  -> 6월
 //날짜 시간 지정 :년도,월,일,시,분,초
 console.log(new Date(2022,02,16,9,30,25))  //Wed Mar 16 2022 09:30:25 GMT+0900(3월)
-//getTime 메소드  : 1970년 1월1일 기준으로 지나온 시간을 ms 단위로 계산하여 리턴
+//getTime 메소드  : 1970년 1월1일 기준으로 지나온 시간을 ms 단위로 계산하여 리턴- 타임스탬프(timestamp)
 console.log(new Date().getTime())
 //세계표준시
 console.log(new Date().toUTCString())
@@ -51,3 +51,10 @@ const date2 = today2.getDate().toString().padStart(2,0);
 console.log([year,month2,date2].join(''))
 console.log([year,month2,date2].join('-'))
 //기억하기 : 배열 -> 문자열로 join , 문자열->배열로 split
+
+console.log(new Date('2022-03-03'))
+console.log(new Date('2022-03-06')-new Date('2022-03-03'))      //결과는 타임스탬프(timestamp)값
+let diff = new Date('2022-03-06')-new Date('2022-03-03');
+const oneday_timestamp = 1*24*60*60*1000;           //1일 = 24시간*60분*60초*1000밀리초
+console.log(oneday_timestamp)       //1일 = 86400000밀리초
+console.log(diff/oneday_timestamp)      //두 날짜의 간격을 '일'로 구합니다.
